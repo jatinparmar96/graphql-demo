d3a2c9a262f9eaf0482dd72be658f7d75051cabc
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-10-24 17:03:40 -0700

    Feat: Optionally show cars form

--- client/src/components/layout/MainContent.js
+++ client/src/components/layout/MainContent.js
@@ -1,5 +1,7 @@
+import { useQuery } from "@apollo/client";
 import { Segmented } from "antd";
 import { useState } from "react";
+import { GET_PERSONS } from "../../queries";
 import AddCar from "../forms/AddCar";
 import AddPerson from "../forms/AddPerson";
 import PersonList from "../lists/PersonList";
@@ -22,21 +24,18 @@ const MainContent = () => {
   const [current, setCurrent] = useState("person");
 
   const styles = getStyles();
+  const { loading, data } = useQuery(GET_PERSONS);
   const onSegmentChange = (e) => {
     setCurrent(e);
   };
+  const options = [{ label: "Person", value: "person", key: "person" }];
+  if (!loading && data.persons.length) {
+    options.push({ label: "Car", value: "car", key: "car" });
+  }
   return (
     <div style={styles.container}>
-      <Segmented
-        value={current}
-        onChange={onSegmentChange}
-        options={[
-          { label: "Person", value: "person", key: "person" },
-          { label: "Car", value: "car", key: "car" },
-        ]}
-      />
+      <Segmented value={current} onChange={onSegmentChange} options={options} />
       {current === "person" ? <AddPerson /> : <AddCar />}
-
       <PersonList />
     </div>
   );
